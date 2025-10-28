920c4febc6324abb21f54eee72235436613c2fe1
Anna-Yevtushenko <ayevtushenko@kse.org.ua>
2025-10-28 16:38:09 +0200

    Added String Boot app for Cosmo Cats Market project

---
+++ cosmo-cats-market/cosmo-cats-market/src/main/java/com/example/cosmocatsmarket/CosmoCatsMarketApplication.java
@@ -0,0 +1,14 @@
+package com.example.cosmocatsmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmoCatsMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmoCatsMarketApplication.class, args);
+		System.out.println("Hello world!");
+	}
+
+}
---
+++ cosmo-cats-market/cosmo-cats-market/src/test/java/com/example/cosmocatsmarket/CosmoCatsMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cosmocatsmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmoCatsMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
