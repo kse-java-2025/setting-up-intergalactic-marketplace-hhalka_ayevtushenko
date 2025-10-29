db0088c6c50aed4f3eac5da39ada0f429260b0c1
Hanna Halka <hanna.dm.galka@gmail.com>
2025-10-29 22:32:20 +0200

    mapper

--- cosmo-cats-market/cosmo-cats-market/src/main/java/com/example/cosmocatsmarket/mapper/ProductMapper.java
+++ cosmo-cats-market/cosmo-cats-market/src/main/java/com/example/cosmocatsmarket/mapper/ProductMapper.java
@@ -1,8 +1,33 @@
 package com.example.cosmocatsmarket.mapper;
 
-import com.example.cosmocatsmarket.mapper.Product;
+import com.example.cosmocatsmarket.domain.Product;
 import com.example.cosmocatsmarket.dto.*;
+
+import org.mapstruct.*
+import java.util.*
 
 public interface ProductMapper {
 
+    ProductDTO toDto(Product product);
+    List<ProductDTO> toDtoList(List<Product> products);
+
+    @Mapping(target = "categories", ignore = true)
+    Product fromDTO(ProductDTO dto);
+    List<Product> toEntityList(List<ProductDTO> dtoList);
+
+    String toStrFromUuid(UUID id) { // Converting string from UUID
+        if (id == null) {
+            return null;
+        } else {
+            return id.toString();
+        }
+    }
+
+    UUID toUuidFromStr(String s) { // Converting UUID from string
+        if (id == null) {
+            return null;
+        } else {
+            return id.fromString();
+        }
+    }
 }
