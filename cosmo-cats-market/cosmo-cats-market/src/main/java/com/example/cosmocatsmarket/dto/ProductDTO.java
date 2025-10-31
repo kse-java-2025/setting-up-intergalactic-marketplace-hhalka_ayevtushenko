caf6001118f274afccf86c480f67f314028af1a2
Anna-Yevtushenko <ayevtushenko@kse.org.ua>
2025-10-31 09:42:09 +0200

    Updated build.gradle with required dependencies and added missing annotations to ProductDTO

--- cosmo-cats-market/cosmo-cats-market/src/main/java/com/example/cosmocatsmarket/dto/ProductDTO.java
+++ cosmo-cats-market/cosmo-cats-market/src/main/java/com/example/cosmocatsmarket/dto/ProductDTO.java
@@ -1,10 +1,15 @@
 package com.example.cosmocatsmarket.dto;
 
 import java.util.List;
-import lombok.*
-import jakarta.validation.*
+import lombok.*;
+import lombok.Data;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
+import java.util.*;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class ProductDTO {
     private String productId;
@@ -15,7 +20,7 @@ public class ProductDTO {
     @Size(min = 1, message = "Product name must have at least 1 symbvol")
     private String productName;
 
-    @Min(min = 0, message = "Price must be positive")
+    @Min(value = 0, message = "Price must be positive")
     private Double price;
 
     @Size(min = 1, message = "Product must have at least one category")
