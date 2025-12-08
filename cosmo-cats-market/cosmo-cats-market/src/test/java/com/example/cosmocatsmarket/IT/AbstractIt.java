4f67cd06863404783844d1f97de26720a6a1d288
Anna-Yevtushenko <ayevtushenko@kse.org.ua>
2025-12-08 15:07:58 +0200

    Fixed errors

--- cosmo-cats-market/cosmo-cats-market/src/test/java/com/example/cosmocatsmarket/IT/AbstractIt.java
+++ cosmo-cats-market/cosmo-cats-market/src/test/java/com/example/cosmocatsmarket/IT/AbstractIt.java
@@ -1,8 +1,10 @@
 package com.example.cosmocatsmarket.IT;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -27,4 +29,6 @@ public abstract class AbstractIt {
         registry.add("spring.datasource.username", postgres::getUsername);
         registry.add("spring.datasource.password", postgres::getPassword);
     }
+
+
 }
